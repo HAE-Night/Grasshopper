250a84b31595e376d4a4a386d257a1c7e80ca403
Scr47 <smblscr47@163.com>
2022-11-17 14:06:37 +0800

    新增初始化py文件 添加设计师名字
    Signed-off-by: Scr47 <smblscr47@163.com>

---
+++ GhEnglishCode/init__.py
@@ -0,0 +1,9 @@
+# -- coding: utf-8 --
+# -------------------------------
+# @Author : 水密菠罗
+# @Email : smblscr47@163.com
+# -------------------------------
+# @File : init__
+# @Time : 2022/11/17 13:42
+
+designer_database = ['Niko', 'Night', 'Nancy', 'Landon', 'Levi', 'Jiang', 'Claire', 'Bella', 'Lauren', 'Night', 'Mary', 'Bobo', 'John', 'Link', 'Mikey', 'windy', 'kiki', 'Radish', 'Juhair', 'Mohamed Shoman', 'Ivan', 'Hasir', 'Hari', 'Nikki', 'Zubair', 'Riyas', 'Najeeb', 'Roberto', 'Mohamed Gomaa']
