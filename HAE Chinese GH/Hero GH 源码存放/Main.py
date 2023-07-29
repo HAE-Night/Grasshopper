f760afdef773671fe5a80ded413d6b7587883196
SmblScr47 <smblscr47@163.com>
2023-07-29 17:02:41 +0800

    插件组测试发行版 1、插件版本更新

--- HAE Chinese GH/Hero GH 源码存放/Main.py
+++ HAE Chinese GH/Hero GH 源码存放/Main.py
@@ -6,6 +6,6 @@
 import clr
 
 clr.CompileModules(
-    "Tradition v4.1.ghpy", "init__.py", "Eto_tip.py", "Curve_group.py", "Surface_group.py", "Plane_group.py", "Vector_group.py", "Data_group.py",
+    "Tradition v4.2.ghpy", "init__.py", "Eto_tip.py", "Curve_group.py", "Surface_group.py", "Plane_group.py", "Vector_group.py", "Data_group.py",
     "Brep_group.py", "Geometry_group.py", "Object_group.py", "Others_group.py", "Math_group.py", "Display_group.py"
 )
