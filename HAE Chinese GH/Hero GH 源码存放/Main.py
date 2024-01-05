a087767dbb4c65a6db4176c5223a2595314ceb68
SmblScr47 <smblscr47@163.com>
2024-01-05 17:22:32 +0800

    打包文件: 插件源码版本号修改

--- HAE Chinese GH/Hero GH 源码存放/Main.py
+++ HAE Chinese GH/Hero GH 源码存放/Main.py
@@ -1,4 +1,4 @@
- # -*- ecoding: utf-8 -*-
+# -*- ecoding: utf-8 -*-
 # @ModuleName: Main
 # @Author: invincible
 # @Time: 2022/7/8 11:13
@@ -6,5 +6,5 @@
 import clr
 
 clr.CompileModules(
-    "Tradition v4.6.ghpy", "initialization.py", "Eto_tip.py", "Curve_group.py", "Surface_group.py", "Plane_group.py", "Point_group.py", "Data_group.py", "Brep_group.py", "Geometry_group.py", "Object_group.py", "Others_group.py", "Math_group.py", "Display_group.py", "Facade_group.py"
+    "Tradition v4.6.5(plus).ghpy", "initialization.py", "Eto_tip.py", "Curve_group.py", "Surface_group.py", "Plane_group.py", "Point_group.py", "Data_group.py", "Brep_group.py", "Geometry_group.py", "Object_group.py", "Others_group.py", "Math_group.py", "Display_group.py", "Facade_group.py"
 )
